51920e80543285d051297fe876831a1c782c6c06
Gillian Tunney <gbtunney@mac.com>
2025-05-04 02:29:14 -0400

    feat(root): jjunk

--- packages/repo-git-logger/vitest.config.ts
+++ packages/repo-git-logger/vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     test: {
         coverage: {
             all: true,
-            include: ['src/**/*.{ts,js}'],
+            include: ['./src/**/*.{ts,js}'],
             reporter: ['text', 'html'],
         },
         environment: 'node',
